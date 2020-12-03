1f92cc2f6e195aa87eb715e2540177dc644404eb
Sam Matthews <36134497+SamBucaMatthews@users.noreply.github.com>
2020-12-03 21:32:14 +0000

    Updated day 1 to work with part 2

--- Day 1/day1.py
+++ Day 1/day1.py
@@ -1,10 +1,15 @@
-import os
+import math
 import itertools
 
-with open("day-1-input.txt") as input_file:
-    values = [int(l) for l in input_file.read().splitlines()] 
+def product_of_combinations(count_of_entries):
+    for combination in itertools.combinations(values, count_of_entries):
+        if sum(combination) == 2020:
+            result = math.prod(combination)
+            return result
 
-for combination in itertools.combinations(values, 2):
-    if combination[0] + combination[1] == 2020:
-        result = combination[0] * combination[1]
-        print(result)
+
+with open("day-1-input.txt") as input_file:
+    values = [int(l) for l in input_file.read().splitlines()]
+
+print(product_of_combinations(2))
+print(product_of_combinations(3))
