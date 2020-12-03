17db4396b9689b30353f7116eef2591231a0bef6
SamBucaMatthews <36134497+SamBucaMatthews@users.noreply.github.com>
2020-12-03 22:04:04 +0000

    Added day 2 from other machine

---
+++ Day 2/Day2.py
@@ -0,0 +1,18 @@
+with open("input.txt") as file:
+    input = file.read().splitlines()
+
+def is_valid(value):
+    parts = value.split(": ")
+    password = parts[1]
+    search_letter = parts[0].split()[1]
+    search_range = parts[0].split()[0]
+    min = int(search_range.split("-")[0])
+    max = int(search_range.split("-")[1])
+
+    count = password.count(search_letter)
+
+    return count >= min and count <= max
+
+results = [is_valid(i) for i in input]
+
+print(results.count(True))
